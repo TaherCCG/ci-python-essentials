a4d87dda3ad66d9fe08397d79ed00c0531ce43e9
TaherCCG <taher.ccg@gmail.com>
2024-05-01 14:17:47 +0100

    Add print.py file for displaying output and debugging

---
+++ m1-python-specific-core-concepts/t1-displaying-output-and-debugging/print.py
@@ -0,0 +1,20 @@
+# Print function is used to display output on the console.
+
+print("Hello world!")
+
+print("Please", "say", "hello", "back!")
+print("Hi!!")
+
+print("5 + 5")
+print(5+5)
+
+
+# This is a syntax error
+# print("5 + 5 = " 5+5)
+
+# This is not a syntax error
+print("5 + 5 = ", 5+5)
+
+# the print function can take multiple arguments
+print("Hello", "world", "!", sep=" ", end="\n")
+print("Hello", "world", "!", sep=" ", end="")
