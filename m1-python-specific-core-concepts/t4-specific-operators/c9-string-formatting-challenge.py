91b578e50fa51983499e4890ba2875c05cc9b62b
TaherCCG <taher.ccg@gmail.com>
2024-05-01 21:53:36 +0100

    Add string formatting challenge file

---
+++ m1-python-specific-core-concepts/t4-specific-operators/c9-string-formatting-challenge.py
@@ -0,0 +1,16 @@
+"""Results
+your result should look like this:
+Igor is 35
+Igor is 35
+"""
+
+name = "Igor"
+age = 35
+
+# write your code here
+
+concat_string = name + " is " + age
+print(concat_string)
+
+f_string = f"{name} is {age}"
+print(f_string)
