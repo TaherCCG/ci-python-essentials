57d3c0897fbc2e120fd5885b7c3b171ec345c2f3
TaherCCG <taher.ccg@gmail.com>
2024-05-01 21:52:49 +0100

    Add c8-incrementing-decrementing-challenge.py file for  incrementing and decrementing operations in Python challenge

---
+++ m1-python-specific-core-concepts/t4-specific-operators/c8-incrementing-decrementing-challenge.py
@@ -0,0 +1,21 @@
+"""Result
+Your result should look like the following:
+
+100
+2
+100
+1
+
+"""
+
+num=100
+print(num) 
+num//=50
+print(num) 
+
+num_b=100
+print(num_b)
+num_b%=3
+print(num_b)
+
+
