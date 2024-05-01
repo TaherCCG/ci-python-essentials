234657d08600dde1e8e71863ac5fc00a43310a6f
TaherCCG <taher.ccg@gmail.com>
2024-05-01 21:53:11 +0100

    Add storing-values-with-assignment-operators.py file for demonstrating storing values with assignment operators in Python

---
+++ m1-python-specific-core-concepts/t4-specific-operators/storing-values-with-assignment-operators.py
@@ -0,0 +1,20 @@
+# Storing Values With Assignment Operators
+
+# Variables assignment
+
+first_number = 10
+second_number = 5
+
+print("10 + 5")
+print(first_number + second_number)
+
+
+print("first_number = float(10)")
+first_number = float(10)
+
+print("10.0 + 5")
+print(first_number + second_number)
+
+my_number = 5
+my_number = 10
+print(my_number)
