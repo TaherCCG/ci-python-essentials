e1e7fa8814f2d05a458b770252a9036ab0db5596
TaherCCG <taher.ccg@gmail.com>
2024-05-01 14:18:02 +0100

    Add variables.py file for demonstrating the use of variables in Python

---
+++ m1-python-specific-core-concepts/t2-variables/variables.py
@@ -0,0 +1,33 @@
+"""
+Variables in Python
+Variables in Python are used to store data. 
+In Python, variables are created when a value is assigned to them. 
+Variables are used to store data that can be accessed and manipulated later in the program. 
+Variables in Python are case-sensitive, which means that the variable name 'age' is different from 'Age'. 
+Variables in Python can store different types of data, such as integers, strings, lists, and dictionaries. 
+Variables in Python can also be reassigned to different values.
+"""
+
+
+my_number = 5
+print(my_number)
+
+My_Number=10
+print(My_Number)
+
+my_name = 'John'
+print(my_name)
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list)
+
+my_List = [5,4,3,2,1]
+print(my_List)
+
+my_dict = {'name': 'John', 'age': 25}
+print(my_dict)
+
+print(my_dict['name'])
+print(my_dict['age'])
+
+
