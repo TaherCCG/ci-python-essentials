a120269eb8d5023fc1057f7fb840ba174e64fdd4
TaherCCG <taher.ccg@gmail.com>
2024-05-01 19:58:52 +0100

    Add string variables challenge and example files

---
+++ m1-python-specific-core-concepts/t3-specific-data-types/c5-string-variables-challenge.py
@@ -0,0 +1,4 @@
+# define your variable here
+parrot =  "It's not pining, it's passed on! This parrot is no more! It has ceased to be!"
+print(parrot)
+
---
+++ m1-python-specific-core-concepts/t3-specific-data-types/string-variables.py
@@ -0,0 +1,35 @@
+#  String Variables
+
+# 1. Single Quotes
+# 2. Double Quotes
+
+# Single Quotes
+print('Hello')
+
+# Double Quotes
+print("Hello")
+
+
+
+"""
+This will not work
+
+print("Then Mike said "What is that?"")
+print('It's a beautiful day')
+
+They will work if you use the escape character \ before the quotes
+"""
+
+example = "Then Mike said \"What is that?\""
+print(example)
+
+example2 = 'It\'s a beautiful day'
+print(example2)
+
+
+# or you can use as below
+example3 = 'Then Mike said "What is that?"'
+print(example3)
+
+example4 = "It's a beautiful day"
+print(example4)
