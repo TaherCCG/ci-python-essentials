8b5edcf7382d9a6207d11ba71ab58ed137ac61cd
TaherCCG <taher.ccg@gmail.com>
2024-05-01 19:57:34 +0100

    Add c2-determining-data-type-challenge.py file for demonstrating how to determine the data type in Python

---
+++ m1-python-specific-core-concepts/t3-specific-data-types/c2-determining-data-type-challenge.py
@@ -0,0 +1,10 @@
+itm_one = 10
+itm_two = 21.56
+itm_three = 'A string of text'
+itm_four = True
+
+#code written by me
+print(type(itm_one))
+print(type(itm_two))
+print(type(itm_three))
+print(type(itm_four))
