37f8bb6fb569cf57c62d989e95f5b8e34ee51637
TaherCCG <taher.ccg@gmail.com>
2024-05-03 00:42:43 +0100

    feat: Add code for demonstrating truthy and falsy values in Python

---
+++ m1-python-specific-core-concepts/t5-specific-comparators/c13-truthy-falsy-values-challenge.py
@@ -0,0 +1,46 @@
+
+"""Before
+The output should look like the following:
+False
+False
+False
+False
+False
+
+
+Result
+Your result should look like the following:
+
+False
+False
+True
+False
+True
+
+"""
+
+a = None
+b = None
+c = None
+d = None
+e = None
+
+print(bool(a))
+print(bool(b))
+print(bool(c))
+print(bool(d))
+print(bool(e))
+
+# Write your code below
+print("Result")
+a = []
+b = str()
+c = str("none")
+d = float()
+e = int(1)
+
+print(bool(a))
+print(bool(b))
+print(bool(c))
+print(bool(d))
+print(bool(e))
