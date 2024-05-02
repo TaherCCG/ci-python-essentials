338706c3763efc40429df8c9673415b497ca5212
TaherCCG <taher.ccg@gmail.com>
2024-05-03 00:42:56 +0100

    Add file for demonstrating identity vs. equality challenge in Python

---
+++ m1-python-specific-core-concepts/t5-specific-comparators/c14-identity-vs-equality-challenge.py
@@ -0,0 +1,26 @@
+"""Result
+Your result should look like the following:
+
+False
+True
+True
+
+
+Using print()
+print out whether list_a is list_b
+print out whether list_a is equal to list_b
+print out whether list_a is list_c
+
+"""
+
+
+list_a = [10, 20, 30,]
+list_b = [10, 20, 30,]
+list_c = list_a
+
+# Write your code below
+
+
+print(list_a is list_b)
+print(list_a == list_b)
+print(list_a is list_c)
