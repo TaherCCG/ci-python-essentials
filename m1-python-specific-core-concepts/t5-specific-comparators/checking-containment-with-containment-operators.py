29e786c4d8f14fe78bffd69a883a0c4cc953b10d
TaherCCG <taher.ccg@gmail.com>
2024-05-03 00:42:20 +0100

    feat: Add file for checking containment with containment operators in Python

---
+++ m1-python-specific-core-concepts/t5-specific-comparators/checking-containment-with-containment-operators.py
@@ -0,0 +1,31 @@
+# Checking Containment with Containment Operators
+
+"""
+Containment operators are used to check if a sequence is present in an object. It returns either True or False according to the condition.
+
+Python Containment Operators
+
+Operator  |	Description	                                            |  Example
+===========================================================================================
+in	       Returns True if a sequence is present in the object	    |  x in y
+not in	   Returns True if a sequence is not present in the object	|  x not in y
+
+
+The syntax for using it is:
+
+'Program' in 'Programming'
+'Program' not in 'Programming'
+
+"""
+print('Program' in 'Programming')
+print('spam' in ['spam', 'egg'])
+print('sausage' not in ['spam', 'egg'])
+
+
+# other examples
+print('a' in 'programming')
+print('a' not in 'programming')
+print('b' in 'programming')
+print('b' not in 'programming')
+print('c' in 'programming')
+print('c' not in 'programming')
