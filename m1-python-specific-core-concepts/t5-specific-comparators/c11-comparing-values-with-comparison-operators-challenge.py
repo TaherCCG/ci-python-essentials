a59d3737c48b6fad4cce32d08af83f79f397c287
TaherCCG <taher.ccg@gmail.com>
2024-05-02 21:59:38 +0100

    Add comparing-values-with-comparison-operators-challenge.py file for demonstrating comparison operators in Python

---
+++ m1-python-specific-core-concepts/t5-specific-comparators/c11-comparing-values-with-comparison-operators-challenge.py
@@ -0,0 +1,20 @@
+"""Result
+Your result should look like this:
+
+False
+False
+False
+True
+True
+"""
+
+a = 10
+b = 5
+
+# Write your code below
+
+print(a == b)
+print(a<b)
+print(a<=b)
+print(a>b)
+print(a>=b)
