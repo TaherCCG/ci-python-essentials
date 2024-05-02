bbfbf3ed8c12590de1d51b9207c10c715141dab7
TaherCCG <taher.ccg@gmail.com>
2024-05-02 21:59:50 +0100

    Add python-logical-operators-challenge.py file for demonstrating logical operators in Python

---
+++ m1-python-specific-core-concepts/t5-specific-comparators/c12-python-logical-operators-challenge.py
@@ -0,0 +1,21 @@
+"""Result
+Your result should look like the image below.
+
+False
+False
+True
+"""
+
+# Write your code below
+
+a=10
+b=5
+
+result_one = a>b and a>10
+result_two = a==5 or b<5
+result_three = a==10 and b==5
+result_three = not result_two
+
+print(result_one)
+print(result_two)
+print(result_three)
