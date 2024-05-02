04498399c3ea76b8f529552dcb8b1603bb1a75ea
TaherCCG <taher.ccg@gmail.com>
2024-05-02 21:59:32 +0100

    Add comparing-values-with-comparison-operators.py file for demonstrating comparison operators in Python

---
+++ m1-python-specific-core-concepts/t5-specific-comparators/comparing-values-with-comparison-operators.py
@@ -0,0 +1,31 @@
+# Comparing Values With Comparison Operators
+
+""" 
+Comparison operators are used to compare values. It returns either True or False according to the condition.
+
+Python Comparison Operators
+
+Operator  |	Description	              |  Example
+=======================================================         
+<	        Less than	                x < y
+>	        Greater than	            x > y
+<=	        Less than or equal to	    x <= y
+>=	        Greater than or equal to	x >= y
+==	        Equal	                    x == y
+!=	        Not equal	                x != y
+
+"""
+
+print('Hello, World!' == 'Hello, World!')
+print(2!=2)
+print([1,2]<[1,2,3])
+print(float(2)>=int(2))
+print('a'<'A') #This is False as 'a' is Unicode 97 where 'A' is 65
+
+# Other examples
+print(2<3)
+print(2>3)
+print(2<=3)
+print(2>=3)
+print(2==3)
+print(2!=3)
