bccb6bfd95592bc4406b284f9cd77934b7e60aa5
TaherCCG <taher.ccg@gmail.com>
2024-05-03 00:43:04 +0100

    feat: Add code for checking containment with containment operators in Python

---
+++ m1-python-specific-core-concepts/t5-specific-comparators/c15-checking-containment-with-containment-operators-challenge.py
@@ -0,0 +1,18 @@
+"""Result
+Your result should look like the following:
+
+True
+False
+False
+
+Create a print() statement to check if the string "rat" is in the string "crate"
+Create a print() statement to check if the string "ink" is NOT in the string "sink"
+Create a print() statement to check if the string "robbie" is in this list ["gary", "howard", "mark", "jason"]
+
+"""
+
+# Write your code below
+
+print("rat" in "crate")
+print ("ink" not in "sink")
+print("robbie" in ["gary", "howard", "mark", "jason"])
