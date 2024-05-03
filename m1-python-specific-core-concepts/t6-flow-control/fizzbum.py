95f9f13597ecf3a59fa18d4789defc270892bc21
TaherCCG <taher.ccg@gmail.com>
2024-05-03 11:34:55 +0100

    Refactor FizzBuzz code to handle multiple conditions in Python

---
+++ m1-python-specific-core-concepts/t6-flow-control/fizzbum.py
@@ -0,0 +1,27 @@
+# My code:
+
+num = int(input("Please enter a number:"))
+
+if num % 5 == 0 | num % 3 == 0:
+    print("FizzBum")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print(num)
+
+
+# Follow along video code:
+print ("Follow along video")
+    
+num=3
+
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBum")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print(num)
