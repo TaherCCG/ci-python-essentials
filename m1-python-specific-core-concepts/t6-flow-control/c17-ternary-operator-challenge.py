dfe48879cacdc8728bee73f95ca37651794ac5ed
TaherCCG <taher.ccg@gmail.com>
2024-05-03 11:34:03 +0100

    feat: Add ternary operator challenge solution in Python

---
+++ m1-python-specific-core-concepts/t6-flow-control/c17-ternary-operator-challenge.py
@@ -0,0 +1,8 @@
+"""Assign a ternary conditional statement to the variable result, that evaluates to True if the variable full_access is True or False otherwise"""
+
+full_access = True
+result = None
+
+result = True if full_access == True else False
+ 
+print(result)
