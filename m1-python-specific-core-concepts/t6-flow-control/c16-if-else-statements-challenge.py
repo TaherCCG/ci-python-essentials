b33ff5abe21256d967276ba673e9e39c56a89ccf
TaherCCG <taher.ccg@gmail.com>
2024-05-03 11:33:44 +0100

    Add if-else statement challenge in Python challenge

---
+++ m1-python-specific-core-concepts/t6-flow-control/c16-if-else-statements-challenge.py
@@ -0,0 +1,33 @@
+a = 10
+b = 20
+result = None
+
+# Write your if statement here:
+
+if a == b:
+    result = 'a has the same value as b'
+else:
+    result = 'a has not got the same value as b'
+
+print(result)
+
+# Output: a has not got the same value as b
+
+"""
+In this challenge, you need to write an if-else statement to compare the values of two variables a and b.
+If the values of a and b are equal, the result should be 'a has the same value as b'.
+If the values of a and b are not equal, the result should be 'a has not got the same value as b'.
+"""
+
+a = 10
+b = 10
+result = None
+
+# Write your if statement here:
+
+if a == b:
+    result = 'a has the same value as b'
+else:
+    result = 'a has not got the same value as b'
+
+print(result)
