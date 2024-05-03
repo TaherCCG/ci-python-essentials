acde39cd47fb242ebcd64a7644ecef6c54fe5414
TaherCCG <taher.ccg@gmail.com>
2024-05-03 14:52:54 +0100

    Refactor iteration control statements in Python + break and pass challenge

---
+++ m1-python-specific-core-concepts/t7-iteration/c22-controlling-iteration-with-break-challenge.py
@@ -0,0 +1,15 @@
+"""
+1.  Create an infinite while loop
+2.  It should contain an if statement that checks if the variable x is equal to 8 and if so break.
+3.  Otherwise, it should print out the value of x
+4.  It should increment the variable x by 1 each time it loops,
+"""
+
+x=0
+
+while True:
+    if x==8:
+        break
+    print(x)
+    x+=1
+
---
+++ m1-python-specific-core-concepts/t7-iteration/c23-controlling-iteration-with-pass-challenge.py
@@ -0,0 +1,17 @@
+"""
+1.  Create a while loop that runs while the variable x is less than 14
+2.  It should contain an if-else statement
+3.  It should check if x > 4 and x < 11
+4.  If x is between these two limits it should just pass
+5.  else it should print x
+6.  After the if-else, it should increment the variable x by 1
+"""
+
+x = 0
+
+while x <14:
+    if x >4 and x < 11:
+        pass
+    else:
+        print (x)
+    x +=1
