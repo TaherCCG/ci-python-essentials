9f1d5fdf2980b5983b1d3fca9136e5a55edd8feb
TaherCCG <taher.ccg@gmail.com>
2024-05-05 22:23:33 +0100

    feat: Add reserved keywords code example

---
+++ m1-python-specific-core-concepts/t10-best-practices/reserved-keywords.py
@@ -0,0 +1,39 @@
+# Reserved Keywords
+
+"""
+Keywords are reserved words in Python. That means they cannot be used as ordinary identifiers like a variable, function, method or class name. 
+They have a specific meaning and purpose within Python and cannot be used for anything other than that purpose.
+"""
+
+# Python has a set of reserved keywords that cannot be used as variable names, function names, or any other identifiers.
+
+# Here is a list of reserved keywords in Python:
+
+# False      class      finally    is         return
+# None       continue   for        lambda     try
+# True       def        from       nonlocal   while
+# and        del        global     not        with
+# as         elif       if         or         yield
+# assert     else       import     pass
+# break      except     in         raise
+
+# You can use the dir() function to get a list of all the reserved keywords in Python.
+
+print(dir(__builtins__))
+
+# You can also use the keyword module to get a list of all the reserved keywords in Python.
+
+import keyword
+
+print(keyword.kwlist)
+
+# You can use the iskeyword() function to check if a string is a reserved keyword in Python.
+
+print(keyword.iskeyword("for")) # True
+print(keyword.iskeyword("hello")) # False
+
+"""
+If using a keyword in a name cannot be avoided, then you can append a single trailing underscore. 
+However, it is better to find a synonym for the keyword and use that instead.
+"""
+
