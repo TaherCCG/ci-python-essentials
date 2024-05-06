e643faf962be45caaad85a14691330e42de5561b
TaherCCG <taher.ccg@gmail.com>
2024-05-06 23:41:30 +0100

    feat: Add defining classes code example

---
+++ m2-python-functions-object-oriented-programming/t2-classes/defining-classes.py
@@ -0,0 +1,23 @@
+# Defining Classes in Python
+
+"""
+Python is known as an object-oriented language, which means it has first-class support for classes. Unlike some other languages, 
+for instance, Java, you’re not forced to use classes, as you can see by the fact that we haven’t used classes in any of the other 
+units. Python is also known as mixed-paradigm language in that you can use either a functional or object-orientated style. Classes 
+are a good way of combining data and methods and are useful when dealing with hierarchies. They are generally used to model more 
+complex data types which can’t be modelled using Python’s built-in data structures such as lists and dicts.
+
+
+We define a class by using the class keyword followed by the name. Like functions, a class does not do anything until it is executed.
+Scope also applies to classes so global and nonlocal scope have to be taken into consideration. A class name should start with a 
+capital letter and should have a docstring. Classes can contain functions and other statements.
+
+class HelloWorld:
+
+
+"""
+"""A simple example class"""
+i = 12345
+def f(self):
+    return 'Hello, world!'
+
